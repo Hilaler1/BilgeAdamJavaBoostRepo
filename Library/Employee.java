f5208d9d32fdbd178870115981271ca1ff7b167a
Hilaler1 <111269960+Hilaler1@users.noreply.github.com>
2022-09-18 15:43:30 +0300

    Add files via upload

---
+++ Library/Employee.java
@@ -0,0 +1,24 @@
+package Library;
+
+public class Employee extends Person {
+
+	@Override
+	public void helloPage() {
+		System.out.println("1- Add book ");
+		System.out.println("2- Release book ");
+		System.out.println("3- Change password ");
+		
+	}
+	
+
+	
+	
+	
+
+	
+	
+	
+	
+
+
+}
